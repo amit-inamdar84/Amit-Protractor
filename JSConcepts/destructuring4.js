30252653bfd7cf63085a8ba32a3998bc46b193a5
Amit <amit.inamdar84@gmail.com>
2021-06-15 16:39:04 +0530

    Latest javascript learning files

--- JSConcepts/destructuring4.js
+++ JSConcepts/destructuring4.js
@@ -1,5 +1,5 @@
 //Example on when to use array destructuring.
-//The requirement is to map the values in ponits array to x and y coordinates and make another array of object.
+//The requirement is to map the values in points array to x and y coordinates and make another array of object.
 
 const points = [
    [4,5],
